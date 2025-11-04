10906f2b12424d741791a525e2714cb5654bb5a2
Taha ManaquibWala <taha.manaquib53@gmail.com>
2025-11-04 14:27:55 +0530

    Array basics

---
+++ src/10_array_basics.ts
@@ -0,0 +1,6 @@
+const nums: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]; // T[]
+const strs: Array<string> = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"]; // Array<T>
+
+const mix: (string | number)[] = [1, "a", 2, "b", 3, "c", 4, "d", 5, "e"]; // (T | U)[]
+
+const mix2: Array<string | number> = [1, "a", 2, "b", 3, "c", 4, "d", 5, "e"]; // Array<T | U>
